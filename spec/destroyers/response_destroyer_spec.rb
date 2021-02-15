6a72c779ecd2da846c7f4fbbed2aac9b1bbae48c
Kevin Cooper <cooperka92@gmail.com>
2021-02-14 23:39:50 -0800

    🚧

--- spec/destroyers/response_destroyer_spec.rb
+++ spec/destroyers/response_destroyer_spec.rb
@@ -4,23 +4,58 @@ require "rails_helper"
 
 describe ResponseDestroyer do
   let(:ability) { Ability.new(user: create(:admin), mission: get_mission) }
-  let(:result) { described_class.new(scope: scope, ability: ability).destroy! }
+  let(:result) {
+    res = described_class.new(scope: scope, ability: ability).destroy!
+    puts "Done"
+    res
+  }
 
   context "with responses" do
     let(:form) do
-      create(:form, question_types: ["select_one", "select_multiple", %w[integer integer], "image"])
+      create(:form, question_types: [
+        "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one",
+        "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one",
+        "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one",
+        "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one",
+        "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one",
+        "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one",
+        "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one",
+        "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one",
+        "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one",
+        "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one",
+        "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one",
+        "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one",
+        "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one", "select_one",
+        "select_multiple", %w[integer integer], "image"])
     end
     let!(:responses) do
       # We don't use create_list because that wouldn't create new images each time.
       Array.new(4) do
-        create(:response, form: form, answer_values: ["Cat", %w[Cat Dog], [1, 2], create(:media_image)])
+        create(:response, form: form, answer_values: [
+          "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat",
+          "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat",
+          "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat",
+          "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat",
+          "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat",
+          "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat",
+          "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat",
+          "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat",
+          "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat",
+          "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat",
+          "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat",
+          "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat",
+          "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat", "Cat",
+          %w[Cat Dog], [1, 2], create(:media_image)])
       end
     end
     let(:decoy) { responses[0] }
     let(:scope) { Response.where.not(id: decoy.id) }
 
+    # TODO: Validate that it runs fast enough.
     it "ignores decoy, deletes others completely" do
+      now = Time.zone.now
       expect(result).to eq(destroyed: 3, skipped: 0, deactivated: 0)
+      puts "@@@ Took: #{Time.zone.now - now}"
       expect(Response.count).to eq(1)
       expect(Answer.count).to eq(5)
       expect(ResponseNode.count).to eq(7)
